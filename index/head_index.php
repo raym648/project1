061032d936c79523c82d5baef68e8cf0896a7d67
raym648 <raymond.francius@gmail.com>
2020-03-20 12:22:13 +0100

    notre site est maintenant full PHP

---
+++ index/head_index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Team Tree House</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Structure of an PHP page">
+    <meta name="keywords" content="PHP, CSS">
+    <meta name="author" content="Raym648">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://kit.fontawesome.com/1c8d8e89d7.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,423;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,423;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+</head>
+
+
---
+++ team/head_team.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Treehouse for Teams</title>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,423;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,423;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/reset.css" type="text/css" />
+    <link rel="stylesheet" href="./css/team-style.css" type="text/css" />
+    <script src="https://kit.fontawesome.com/1c8d8e89d7.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
